1620762026880c4734ae12a8a7509eec600a1dfd
Apep_S <salamilmubin@gmail.com>
2021-03-10 10:38:34 +0700

    menambahkan model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    judul = models.CharField(max_length=200)
+    body =  models.TextField()
+    categori = models.CharField(max_length=250)
+    waktuPost = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}".format(self.judul)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+def index(request):
+    post = Post.objects.all()
+    context = {
+        'Judul':'MyBlog',
+        'Heading':'Ini Adalah Blog',
+        'Posts':post 
+    }
+    return render(request,'blog/index.html',context)
+
